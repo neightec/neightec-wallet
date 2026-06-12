f8d89aac4b45859660d18e96a0ff0c228061d260
bajahitam29 <n.susantoputra@gmail.com>
2026-06-12 07:44:34 +0200

    adjustments: - header adjusted - dark theme from angular material used

--- frontend/src/app/component/header/header.component.ts
+++ frontend/src/app/component/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'neightec-header',
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
