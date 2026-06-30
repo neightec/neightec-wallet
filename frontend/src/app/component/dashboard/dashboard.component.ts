b39c81993bd3fe578dabdfda03dfdb6f9a52dd6c
bajahitam29 <n.susantoputra@gmail.com>
2026-06-30 21:38:50 +0200

    add dummy mat-card

--- frontend/src/app/component/dashboard/dashboard.component.ts
+++ frontend/src/app/component/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'neightec-dashboard',
-  imports: [],
+  imports: [
+    MatCardModule, MatButtonModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
